e925f9584f828ed7ad5fc55864f767b261c74575
Miguel Díaz <miguel.diaz1999@gmail.com>
2025-01-03 15:12:38 +0100

    Added disassemble flag

--- README.md
+++ README.md
@@ -1,4 +1,18 @@
 # nes-go
 Nes emulator written in go. Work in progress.
 
+## Usage
+
+Run emulator:
+
+```bash
+./nes-go <rom path>
+```
+
+Run disassembler:
+
+```bash
+./nes-go -disassemble <rom path>
+```
+
 Source: https://www.nesdev.org/wiki/Nesdev_Wiki
--- main.go
+++ main.go
@@ -1,13 +1,27 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"nes-go/mos6502"
 	"os"
 )
 
 func main() {
-	cart, err := os.ReadFile("nestest.nes")
+	disassemble_activated := flag.Bool("disassemble", false, "Run disassembler")
+	flag.Parse()
+
+	flag_tail := flag.Args()
+
+	var rom_path string
+
+	if len(flag_tail) == 0 {
+		rom_path = "nestest.nes"
+	} else {
+		rom_path = flag_tail[0]
+	}
+
+	cart, err := os.ReadFile(rom_path)
 
 	if err != nil {
 		log.Fatalf("Error reading cartridge: %v", err)
@@ -16,6 +30,10 @@ func main() {
 	rom := mos6502.NewRom(cart)
 	cpu := mos6502.NewCPU(rom)
 
-	disassembler := mos6502.NewDisassembler(cpu)
-	disassembler.DisassembleWeb()
+	if *disassemble_activated {
+		disassembler := mos6502.NewDisassembler(cpu)
+		disassembler.DisassembleWeb()
+	} else {
+		cpu.Run()
+	}
 }
