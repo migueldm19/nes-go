6135212f45426248518ac462181bc2894d993fa1
Miguel Díaz <miguel.diaz1999@gmail.com>
2025-07-22 18:14:48 +0200

    constants

--- emulator/rom.go
+++ emulator/rom.go
@@ -2,6 +2,13 @@ package emulator
 
 import "log"
 
+const(
+	HEADER_SIZE = 16
+	PRG_BYTES_UNITS = 16
+	CHR_BYTES_UNITS = 8
+	BYTES_IN_KILOBYTES = 1024
+)
+
 type Rom struct {
 	PrgRomSize uint16
 	ChrRomSize uint16
@@ -10,13 +17,15 @@ type Rom struct {
 }
 
 func NewRom(cartridge []byte) *Rom {
-	header := cartridge[:16]
-	var prgSize uint16 = uint16(header[4]) * 16384
-	var chrSize uint16 = uint16(header[5]) * 8192
+	header := cartridge[:HEADER_SIZE]
+	var prgSize uint16 = uint16(header[4]) * PRG_BYTES_UNITS * BYTES_IN_KILOBYTES
+	var chrSize uint16 = uint16(header[5]) * CHR_BYTES_UNITS * BYTES_IN_KILOBYTES
 
-	start_chr := 16 + prgSize
+	// TODO: read header and add trainer
 
-	prgData := cartridge[16:start_chr]
+	start_chr := HEADER_SIZE + prgSize
+
+	prgData := cartridge[HEADER_SIZE:start_chr]
 
 	if prgSize >= 0x4000 {
 		prgData = append(prgData, prgData...)
