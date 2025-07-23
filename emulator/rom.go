9c93115c7b4beec2ef41bfa785503cbc7772e05c
Miguel Díaz <miguel.diaz1999@gmail.com>
2025-07-23 20:28:04 +0200

    fix prgData

--- emulator/rom.go
+++ emulator/rom.go
@@ -35,8 +35,8 @@ func getBit(val byte, idx int) bool {
 
 func NewRom(cartridge []byte) *Rom {
 	header := cartridge[:HEADER_SIZE]
-	var prgSize uint16 = uint16(header[4]) * PRG_BYTES_UNITS * BYTES_IN_KILOBYTES
-	var chrSize uint16 = uint16(header[5]) * CHR_BYTES_UNITS * BYTES_IN_KILOBYTES
+	var prgSize = int(header[4]) * PRG_BYTES_UNITS * BYTES_IN_KILOBYTES
+	var chrSize = int(header[5]) * CHR_BYTES_UNITS * BYTES_IN_KILOBYTES
 
 	var ntArrangement NametableArrangement
 	flags6 := header[6]
@@ -53,9 +53,15 @@ func NewRom(cartridge []byte) *Rom {
 		trainer = cartridge[HEADER_SIZE : HEADER_SIZE+TRAINER_SIZE]
 	}
 
-	startChr := HEADER_SIZE + prgSize
+	startChr := startPrg + prgSize
 
 	prgData := cartridge[startPrg:startChr]
+
+	// TODO: why?
+	if len(prgData) >= 0x4000 {
+		prgData = append(prgData, prgData...)
+	}
+
 	chrData := cartridge[startChr : startChr+chrSize]
 
 	if len(chrData) != CHR_DATA_SIZE {
@@ -63,8 +69,8 @@ func NewRom(cartridge []byte) *Rom {
 	}
 
 	return &Rom{
-		PrgRomSize:    prgSize,
-		ChrRomSize:    chrSize,
+		PrgRomSize:    uint16(prgSize),
+		ChrRomSize:    uint16(chrSize),
 		PrgData:       prgData,
 		ChrData:       chrData,
 		Trainer:       trainer,
